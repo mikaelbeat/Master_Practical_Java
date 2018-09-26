6503c81934369fbd900d99cf125c7e38fd171462
Ryynanen, Petri <petri.ryynanen@accenture.comgit clone https://petri.ryynanen@innersource.accenture.com/scm/afhrtvkn/hrrt-test-scripts.gitgit config --global user.email petri.ryynanen@accenture.com>
2018-09-26 17:26:08 +0300

    Update

---
+++ Master_Practical_Java/src/thread_Pools/MessageProcessor.java
@@ -0,0 +1,27 @@
+package thread_Pools;
+
+public class MessageProcessor implements Runnable{
+
+	private int message;
+	
+	public MessageProcessor(int message) {
+		this.message = message;
+	}
+
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + "[RECEIVED] Message = " + message);
+		respondToMessage(); // Make thread sleep to simulate doing some work
+		System.out.println(Thread.currentThread().getName() + "[DONE] Processing message = " + message);
+		
+	}
+
+	private void respondToMessage() {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			System.out.println("Unable to process message: " + message);
+		}
+	}
+	
+}
---
+++ Master_Practical_Java/src/thread_Pools/ThreadPoolDemo.java
@@ -0,0 +1,33 @@
+package thread_Pools;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolDemo {
+	
+	public static void main(String[] args) {
+		
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		
+		Runnable processor1 = new MessageProcessor(2);
+		executor.execute(processor1);
+		
+		Runnable processor2 = new MessageProcessor(3);
+		executor.execute(processor2);
+		
+		Runnable processor3 = new MessageProcessor(4);
+		executor.execute(processor3);
+		
+		Runnable processor4 = new MessageProcessor(5);
+		executor.execute(processor4);
+		
+		executor.shutdown(); // Rejects any new tasks from being submitted. Gracefully shuts down the service
+		
+		while(!executor.isTerminated()) {
+			
+		}
+		
+		System.out.println("Submitted all tasks...");
+	}
+
+}
